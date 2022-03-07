2f3988acd00c5fbfd8538aa4f0b312b0e44b7549
Foreverddb <835236331@qq.com>
2022-03-07 20:39:09 +0800

    安全起见，删除核心代码

--- app/src/main/java/fucklegym/top/entropy/UserInfoNotInitalized.java
+++
@@ -1,7 +0,0 @@
-package fucklegym.top.entropy;
-
-public class UserInfoNotInitalized extends Exception{
-    public String toString(){
-        return "User information not initialized, please initialize it first";
-    }
-}
